4a32bb9e1c57a59f4e71bcb29bde000d04118dd6
55yong <dhdydwns988@gmail.com>
2024-07-28 03:26:30 +0900

    Navbar 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,37 +5,54 @@ import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
   let navigate = useNavigate();
 
-  const handleClick = location => {
+  const handleClick = (e, location) => {
+    e.preventDefault();
     navigate(location);
     window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   };
 
   return (
     <div className="navbar">
-      <div className="navbar__box" onClick={() => handleClick(`cal`)}>
+      <a
+        href="cal"
+        className="navbar__box"
+        onClick={e => handleClick(e, `cal`)}
+      >
         <img className="navbar__box__button" src="cal.png" alt="캘린더" />
         <span className="navbar__box__text">캘린더</span>
-      </div>
-      <div className="navbar__box" onClick={() => handleClick(`insight`)}>
+      </a>
+      <a
+        href="insight"
+        className="navbar__box"
+        onClick={e => handleClick(e, `insight`)}
+      >
         <img className="navbar__box__button" src="insight.png" alt="통계" />
         <span className="navbar__box__text">통계</span>
-      </div>
-      <div className="navbar__box" onClick={() => handleClick(`/`)}>
+      </a>
+      <a href="/" className="navbar__box" onClick={e => handleClick(e, `/`)}>
         <img className="navbar__box__button" src="home.png" alt="홈" />
         <span className="navbar__box__text">홈</span>
-      </div>
-      <div className="navbar__box" onClick={() => handleClick(`community`)}>
+      </a>
+      <a
+        href="community"
+        className="navbar__box"
+        onClick={e => handleClick(e, `community`)}
+      >
         <img
           className="navbar__box__button"
           src="community.png"
           alt="커뮤니티"
         />
         <span className="navbar__box__text">커뮤니티</span>
-      </div>
-      <div className="navbar__box" onClick={() => handleClick(`rank`)}>
+      </a>
+      <a
+        href="rank"
+        className="navbar__box"
+        onClick={e => handleClick(e, `rank`)}
+      >
         <img className="navbar__box__button" src="rank.png" alt="랭킹" />
         <span className="navbar__box__text">랭킹</span>
-      </div>
+      </a>
     </div>
   );
 };
