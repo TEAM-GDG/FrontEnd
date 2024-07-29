efe53b73e8fcbf22e9fd0217f73a2e07a7c9edf8
55yong <dhdydwns988@gmail.com>
2024-07-30 00:06:17 +0900

    리듀서 기본 설정

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
 root.render(
   <BrowserRouter>
     <App />
